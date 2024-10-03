637a8103fa151f209482cb262c5c5dd7c1b93759
ginas1308 <92017631+ginas1308@users.noreply.github.com>
2024-10-03 17:06:56 +0800

    Update customed_reserve.py

--- utils/customed_reserve.py
+++ utils/customed_reserve.py
@@ -8,21 +8,8 @@ class customedreserve(slidereserve):
     name = "custom"
     def __init__(self, sleep_time=0.2, max_attempt=50, reserve_next_day=False):
         super().__init__(sleep_time=sleep_time, max_attempt=max_attempt, reserve_next_day=reserve_next_day)
-        
-        '''
-        for some school with https://office.chaoxing.com/front/apps/seatengine url prefix. **Please modify seatId**
-        eg: 中国矿业大学
-        '''
         self.url = "https://office.chaoxing.com/front/apps/seatengine/select?id={}&day={}&backLevel=2&seatId=093"
         self.submit_url = "https://office.chaoxing.com/data/apps/seatengine/submit"
-
-        '''
-        for some school with https://reserve.chaoxing.com/front/third/apps/seat/select url prefix. **Please modify pageToken**
-        eg: 黄山学院(目前移植到huangshan_customed_reserve.py)
-        '''
-        # self.url = "https://reserve.chaoxing.com/front/third/apps/seat/select?deptIdEnc=ab9abb08fee54ec6&id={}&day={}&backLevel=2&pageToken=9bf6d81424514f49852b2c61966b70d9"
-        # self.submit_url = "https://reserve.chaoxing.com/data/apps/seat/submit?"
-
     def _get_page_token(self, url):
         logging.info(url)
         response = self.requests.get(url=url, verify=False)
